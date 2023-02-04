7f16998d9e35f4552512278d5bb1efcdb2f07ea3
JackofAllTradez7 <57196083+JackofAllTradez7@users.noreply.github.com>
2023-02-04 00:11:26 -0500

    coom

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -6,10 +6,6 @@ using UnityEngine.SceneManagement;
 public class ButtonScript : MonoBehaviour
 {
     // Start is called before the first frame update
-   public void Play()
-    {
-        SceneManager.LoadScene("SampleScene 2 - the scenening");
-    }
     public void GoToScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
